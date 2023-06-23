14fc0236dcd68eff3292c89d14047ab6bfd4bb54
kamide28 <ghmaho0928@gmail.com>
2023-06-23 17:32:16 +0900

    import文再修正

--- src/test/java/com/example/ninthhomework/controller/CharacterListControllerTest.java
+++ src/test/java/com/example/ninthhomework/controller/CharacterListControllerTest.java
@@ -24,7 +24,10 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
